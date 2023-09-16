13f816c6b8649adbbd2b1db0a69323f98f04eb95
Arif islam <136179990+arifislam-26@users.noreply.github.com>
2023-09-16 12:10:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// jq
+ $(document).ready(function(){
+   // alert
+	$('.salert').on('click', function() {
+		alert('Welcome');
+	})
+
+	// show/hide
+
+	$('#show').on( 'click', function(){
+		$('#h2').show();
+	})
+
+	$('#hide').on( 'click', function(){
+		$('#h2').hide();
+	})
+
+})
